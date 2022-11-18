21c4e5adfc043fb9514efa690ce0b9d721a05d6c
vrtex <kuba.jakub17@gmail.com>
2022-11-19 00:28:24 +0100

    Hermes tranlator

---
+++ Source/Hermes/Private/HermesMessageTranslator.cpp
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HermesMessageTranslator.h"
+
+FHermesMessage UHermesMessageTranslator::TranslateMessage_Implementation(const FHermesMessage& OriginalMessage) const
+{
+	unimplemented();
+	return OriginalMessage;
+}
+
+FHermesMessage UHermesMessageTranslator::InvokeMessageTranslator(const FHermesMessage& OriginalMessage, const TSubclassOf<UHermesMessageTranslator>& TranslatorClass)
+{
+	check(TranslatorClass);
+	const UHermesMessageTranslator* TranslatorCDO = TranslatorClass.GetDefaultObject();
+	return TranslatorCDO->TranslateMessage(OriginalMessage);
+}
---
+++ Source/Hermes/Public/HermesMessageTranslator.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "HermesMessage.h"
+#include "UObject/Object.h"
+#include "HermesMessageTranslator.generated.h"
+
+/**
+ * helper class for creating message from a static context
+ * can be used through CDO
+ */
+UCLASS(Abstract, Blueprintable, BlueprintType)
+class HERMES_API UHermesMessageTranslator : public UObject
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	FHermesMessage TranslateMessage(const FHermesMessage& OriginalMessage) const;
+
+	static FHermesMessage InvokeMessageTranslator(const FHermesMessage& OriginalMessage, const TSubclassOf<UHermesMessageTranslator>& TranslatorClass);
+};
+
+// useful when you need to produce a message through a translator and you need the original message
+UCLASS()
+class UHermesMessageTranslator_PassThrough : public UHermesMessageTranslator
+{
+	GENERATED_BODY()
+public:
+	virtual FHermesMessage TranslateMessage_Implementation(const FHermesMessage& OriginalMessage) const override { return OriginalMessage; }
+};
